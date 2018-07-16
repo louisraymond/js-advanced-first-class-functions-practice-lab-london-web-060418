52580fcf67ff6c8a193d8dd551b8e190190d5da2
Louis Raymond <louis.raymond@alumni.york.ac.uk>
2018-07-16 15:04:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+
+
+
+
+const logDriverNames=function (array){
+  array.forEach(function (x){
+    console.log( x.name)
+  }) 
+}
+
+const logDriversByHometown= function(array,location){
+  array.forEach(function (driver) {
+    if (driver.hometown===location) {
+      console.log(driver.name)
+    }
+  })
+}
+
+
+const driversByRevenue= function(array){
+  return array.slice().sort(function (a, b) {
+    return a.revenue - b.revenue;
+  });
+}
+
+const driversByName = function (drivers) {
+  return drivers.slice().sort(function (driverOne, driverTwo) {
+    return driverOne.name.localeCompare(driverTwo.name);
+  });
+};
+
+const totalRevenue = function (drivers) {
+  return drivers.reduce(function (total, currentDriver) {
+    return currentDriver.revenue + total;
+  }, 0);
+};
+
+
+const averageRevenue = function (drivers) {
+  return totalRevenue(drivers) / drivers.length;
+};
