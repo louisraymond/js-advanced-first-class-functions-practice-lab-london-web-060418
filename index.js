5565e77de5a9c975bf2c26185b184c18e30ac7e0
Louis Raymond <louis.raymond@alumni.york.ac.uk>
2018-07-16 15:01:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+
+
+
+
+const logDriverNames=function (array){
+  array.forEach(function (x){
+    console.log( x.name)
+  }) 
+}
+
+const logDriversByHometown= function(array,location){
+  array.forEach(function (driver) {
+    if (driver.hometown===location) {
+      console.log(driver.name)
+    }
+  })
+}
+
+
+const driversByRevenue= function(array){
+  return array.slice().sort(function (a, b) {
+    return a.revenue - b.revenue;
+  });
+}
+
+
+const
